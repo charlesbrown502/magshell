4501113aad260790481c591f3ba78ebabbfef0c2
yayanksenpai <84301128+yayanksenpai@users.noreply.github.com>
2021-07-09 12:22:30 +0700

    Check app link var

--- alive.sh
+++ alive.sh
@@ -1,5 +1,7 @@
-while true
-do
-    sleep 25m
-    wget -q -O/dev/null $APP_LINK
-done
+if [[ -n $APP_LINK ]]; then
+    while true
+    do
+        sleep 25m
+        wget -q -O/dev/null $APP_LINK
+    done
+fi
